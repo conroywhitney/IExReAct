ca7c639bf5c1326521c6f45646ac5ee83c62d0bc
Conroy Whitney <conroy.whitney@gmail.com>
2026-01-11 02:56:27 -0500

    Set up IExReAct project scaffolding
    - Initialize Mix project with jido_ai dependency
    - Add runtime config for dotenvy .env loading
    - Create vault/ directory as safe zone for agent file ops
    - Prepare empty test structure for TDD

---
+++ lib/iex_react.ex
@@ -0,0 +1,5 @@
+defmodule IExReAct do
+  @moduledoc """
+  A minimal ReAct agent for IEx demonstrating safe tool use.
+  """
+end
---
+++ test/iex_react_test.exs
@@ -0,0 +1,5 @@
+defmodule IExReActTest do
+  use ExUnit.Case
+
+  # Tests will be added via TDD
+end
